f45d3010b8c4e07f1d8022047469d368188f2aaf
abedalkareem omreyh <abedalkareem.omreyh@yahoo.com>
2020-05-26 19:18:45 +0400

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by abedalkareem omreyh on 26/05/2020.
+//  Copyright © 2020 abedalkareem omreyh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
